de1c887baec538682c95d6ab5450a598653b1df3
Maria Nanfuka <mariah.kakande@gmail.com>
2018-06-23 16:53:53 +0300

    Add API Endpoints

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class Rideoffer:
+    def __init__(self,id,driver,destination):
+        self.id =id
+        self.driver = driver
+        self.destination = destination
+
+
+    def get_dict(self):
+        return{
+            'id':self.id,
+            'driver':self.driver,
+            'destination':self.destination
+        }
+   
---
+++ app/views.py
@@ -0,0 +1,23 @@
+from flask import Flask, request,jsonify
+from .models import Rideoffer
+from run import app
+import json 
+
+@app.route('/api/v1/rides' ,methods= ['POST'])
+def create_rideoffer():
+	""" This endpoint creates a ride offer """
+	user = []
+	data = request.get_json()
+	try:
+		if isinstance(data['driver'], str) and isinstance(data['destination'], str):
+			id = len(user)
+			id += 1 
+			Request =Rideoffer(id,data['driver'],data['destination'] )
+			user.append(Request)  
+		return jsonify(Request.get_dict()), 201
+	#Add an Attribut error to catch the errors
+	except AttributeError:
+		return jsonify({
+			'status': 'FAIL',
+			'message': 'Failed to create a request. Invalid data'
+		}), 400
