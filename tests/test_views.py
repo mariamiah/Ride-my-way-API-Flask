9a9f9cf03f333b071f0ab4e85322310346f50966
maria nanfuka <mariah.kakande@gmail.com>
2018-07-02 15:18:37 +0300

    Update test_views.py file

--- tests/test_views.py
+++ tests/test_views.py
@@ -2,6 +2,7 @@ import json
 import flask
 from flask import jsonify
 from app.views import app
+from app.models import RideOffer
 import unittest
 
 #import requests
@@ -9,7 +10,7 @@ import unittest
 class TestClass(unittest.TestCase):
 
     def setUp(self):
-        self.client = app.test_client
+        self.client = app.test_client()
         
 
     # #create a rideoffer
@@ -21,7 +22,7 @@ class TestClass(unittest.TestCase):
                 }
             )
 
-            response = self.client().post('/api/v1/rides', 
+            response = self.client.post('/api/v1/rides', 
             content_type = 'application/json', data =json.dumps(post_data))
             
             print(response.data)
@@ -33,13 +34,26 @@ class TestClass(unittest.TestCase):
            
             self.assertEqual(response.status_code, 201 )
         ##Fetch all rides
-    def test_fetch_rideoffer(self):
-        response= self.client().get('/api/v1/rides',
+    def test_fetch_all_rides(self):
+        response= self.client.get('/api/v1/rides',
         content_type='application/json')
         print(response.data)
         self.assertEqual(response.status_code,200)
+        
+        
+        ##Fetch a single ride offer
+    def test_fetch_rideId(self):
+        response= self.client.get('/api/v1/rides/{}'.format(1),
+        content_type='application/json')
+        print(response.data)
         self.assertEqual(response.status_code,200)
         
-
+       ##make a ride request
+    def test_request_ride_id(self):
+        response=self.client.post('/api/v1/rides/{}/requests'.format(1),
+        content_type='application/json')
+        print(response.data)
+        self.assertEqual(response.status_code,200)
+        
 if __name__ == "__main__":
     unittest.main()
