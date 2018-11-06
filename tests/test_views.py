1c0f7445f23863194e8ab04e79fb538b4a69ec23
mariamiah <mariah.kakande@gmail.com>
2018-11-06 10:58:00 +0300

    Update test_views.py

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,7 +6,7 @@ import unittest
 class TestClass(unittest.TestCase):
 
     def setUp(self):
-        self.client = app.test_client
+        self.client = app.test_client()
     # create a rideoffer
 
     def test_create_rideoffer(self):
@@ -16,34 +16,30 @@ class TestClass(unittest.TestCase):
                 }
             )
 
-            response = self.client().post('/api/v1/rides',
+            response = self.client.post('/api/v1/rides',
                                           content_type='application/json',
                                           data=json.dumps(post_data))
-            print(response.data)
             self.assertEqual(response.status_code, 201)
 # Fetch all rides
 
 
 def test_fetch_all_rides(self):
-        response = self.client().get('/api/v1/rides',
+        response = self.client.get('/api/v1/rides',
                                      content_type='application/json')
-        print(response.data)
         self.assertEqual(response.status_code, 200)
 # Fetch a single ride offer
 
 
 # def test_fetch_rideId(self):
-        response = self.client().get('/api/v1/rides/<id>',
+        response = self.client.get('/api/v1/rides/<id>',
                                      content_type='application/json')
-        print(response.data)
         self.assertEqual(response.status_code, 200)
 # make a ride request
 
 
 def request_ride_id(self):
-        response = self.client().get('/api/v1/rides/<id>/requests',
+        response = self.client.get('/api/v1/rides/<id>/requests',
                                      content_type='application/json')
-        print(response.data)
         self.assertEqual(response.status_code, 200)
 if __name__ == "__main__":
     unittest.main()
