104c5ac6e67e708b7a060e9ea396f272c3716d93
mariamiah <mariah.kakande@gmail.com>
2021-08-12 05:57:02 +0300

    define user model

--- app/models/users.py
+++ app/models/users.py
@@ -1,26 +1,49 @@
 import sqlalchemy
-from ..app import Base, ma
+from sqlalchemy.ext.associationproxy import association_proxy
+from ..app import db, ma
 
-class User(Base):
-    __tablename__ = 'users'
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
-    first_name = sqlalchemy.Column(sqlalchemy.String(length=100))
-    last_name = sqlalchemy.Column(sqlalchemy.String(length=100))
-    email = sqlalchemy.Column(sqlalchemy.String(length=100))
-    password = sqlalchemy.Column(sqlalchemy.String(length=100))
-    
-    active = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
-
-class UserSchema(ma.Schema):
-    class Meta:
-        # fields to expose
-        fields = ("email", "date_created", "first_name", "last_name", "_links")
-    _links = ma.Hyperlinks(
-        {
-            "self": ma.URLFor("user_detail", values=dict(id="<id>")),
-            "collection": ma.URLFor("users"),
-        }
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=False)
+    username = db.Column(db.String(255), index=True, unique=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255), unique=False)
+    phone_number = db.Column(db.String(255), unique=True)
+    role = db.Column(db.String(255))
+    requested_rels = db.relationship(
+        'Relationship',
+        foreign_keys='Relationship.requesting_user_id',
+        backref='requesting_user' 
     )
+    received_rels = db.relationship(
+        'Relationship',
+        foreign_keys='Relationship.receiving_user_id',
+        backref='receiving_user'
+    )
+    aspiring_friends = association_proxy('received_rels', 'requesting_rels')
+    desired_friends = association_proxy('requested_rels', 'receiving_user')
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+class Relationship(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    requesting_user_id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
+    receiving_user_id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
+    status = db.Column(db.Integer)
+
+class UserSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+
+    id = ma.auto_field()
+    name = ma.auto_field()
+    username = ma.auto_field()
+    email = ma.auto_field()
+    password = ma.autofield()
+    phone_number = ma.autofield()
+    role = ma.auto_field()
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
